791057733b5ca0b54d92c1318b0ea03c2bf5b782
Jules Fouchy <jules.fouchy@ntymail.com>
2024-08-26 20:42:21 +0200

    🔥 Remove interactive launcher code

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 #include "ReleaseManager.hpp"
 #include "download.hpp"
 
-#define INTERACTIVE_LAUNCHER 0
-
 auto main(int argc, char** argv) -> int
 {
     // TODO : faire une fonction!
@@ -29,67 +27,6 @@ auto main(int argc, char** argv) -> int
 
         ReleaseManager release_manager;
 
-#if INTERACTIVE_LAUNCHER
-        if (release_manager.no_release_installed()) // Aucune release d'installée
-        {
-            Release latest_release = release_manager.get_latest_release();
-            release_manager.display_all_release();
-            std::cout << "Aucune release d'installée. \nInstallation automatique de la dernière version : " << latest_release.get_name() << std::endl;
-            int confirm = 0;
-            std::cout << "Lancer l'installation : (1) ";
-            std::cin >> confirm;
-            if (confirm)
-            {
-                release_manager.install_release(latest_release);
-                release_manager.launch_release(latest_release);
-            }
-        }
-        else if (!release_manager.get_latest_release().is_installed()) // des versions installées mais pas la dernière
-        {
-            std::cout << "La dernière version : " << release_manager.get_latest_release().get_name() << " n'est pas installée, mais vous disposez des versions : ....." << std::endl;
-            std::cout << "Souhaitez-vous installer la dernière version ? : " << release_manager.get_latest_release().get_name() << std::endl;
-        }
-        else // La dernière version est au minimum installé. On laisse le choix à l'utilisateur
-        {
-            int choice = 0;
-            std::cout << "Voir les versions de Coollab : (1)" << std::endl;
-            std::cout << "Installer une version : (2)" << std::endl;
-            std::cout << "Lancer une version (3) :" << std::endl;
-            std::cout << "Choix : ";
-            std::cin >> choice;
-
-            if (choice == 1)
-                release_manager.display_all_release();
-            else if (choice == 2)
-            {
-                std::string version_to_install;
-                std::cout << "Choisissez le nom de la version à installer : ";
-                std::cin >> version_to_install;
-
-                for (const Release& release : release_manager.get_all_release())
-                {
-                    if (release.get_name() == version_to_install && !release.is_installed())
-                        release_manager.install_release(release);
-                    else if (release.get_name() == version_to_install)
-                        std::cout << release.get_name() << " est déjà installé !" << std::endl;
-                }
-            }
-            else if (choice == 3)
-            {
-                std::string version_to_launch;
-                std::cout << "Choisissez le nom de la version à lancer : ";
-                std::cin >> version_to_launch;
-
-                for (const Release& release : release_manager.get_all_release())
-                {
-                    if (release.get_name() == version_to_launch && release.is_installed())
-                        release_manager.install_release(release);
-                    else if (release.get_name() == version_to_launch)
-                        std::cout << release.get_name() << " n'est pas installé !" << std::endl;
-                }
-            }
-        }
-#else
         Release const* release_to_launch = nullptr;
 
         // L'utilisateur a déjà un projet sur une version différente de la latest
@@ -108,8 +45,6 @@ auto main(int argc, char** argv) -> int
         release_manager.launch_release(*release_to_launch);
 
         // Problème internet... (TODO)
-
-#endif
     }
     catch (const std::exception& e)
     {

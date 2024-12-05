d33e1db5d964e6c96540f3eb9fdccdaaaeb613ac
Jules Fouchy <jules.fouchy@ntymail.com>
2024-12-05 20:46:55 +0100

    🎉 Start using Cool to make a GUI

---
+++ src/App.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#include "Cool/AppManager/IApp.h"
+#include "Cool/View/ViewsManager.h"
+#include "Cool/Window/WindowManager.h"
+
+class App : public Cool::IApp {
+public:
+    explicit App(Cool::WindowManager& windows, Cool::ViewsManager& views) {}
+
+    void update() override {}
+
+    void imgui_windows() override
+    {
+        ImGui::Begin("Hello");
+        ImGui::End();
+    }
+    void imgui_menus() override {}
+
+private:
+    friend class ser20::access;
+    template<class Archive>
+    void serialize(Archive& /* archive */)
+    {
+    }
+};
+
+// #include <cstdlib>
+// #include <iostream>
+// // #include "ProjectManager/path_to_project_to_open_on_startup.hpp"
+// #include <fstream>
+// #include <string>
+// #include "ReleaseManager.hpp"
+// #include "boxer/boxer.h"
+
+// auto main(int argc, char** argv) -> int
+// {
+//     // TODO : faire une fonction!
+//     std::string version;
+//     if (argc > 1)
+//     {
+//         std::ifstream infile(argv[1]);
+//         if (infile.is_open())
+//             std::getline(infile, version);
+//         infile.close();
+//     }
+
+//     try
+//     {
+// #ifdef __APPLE__
+//         // Install Homebrew & FFmpeg on MacOS
+//         install_macos_dependencies_if_necessary();
+// #endif
+
+//         ReleaseManager release_manager;
+//         // release_manager.display_all_release();
+
+//         Release const* release_to_launch = nullptr;
+
+//         // L'utilisateur a déjà un projet sur une version différente de la latest
+//         // Il a le choix de poursuivre son projet sur sa version, ou de télécharger la latest.
+//         release_to_launch = release_manager.find_release(version);
+//         if (release_to_launch == nullptr)
+//         {
+//             // std::cerr << "La version " << version << " n'a pas pu être installée, installation de la dernière version (par défaut).";
+//             release_to_launch = release_manager.get_latest_release();
+//             if (release_to_launch == nullptr)
+//             {
+//                 boxer::show("Please connect to the Internet so that we can install the latest version of Coollab.\nYou don't have any version installed yet.", "You are offline");
+//                 return 0;
+//             }
+//         }
+
+//         // Si la latest n'est pas installée -> on l'installe
+//         if (!release_to_launch->is_installed())
+//             release_manager.install_release(*release_to_launch);
+
+//         release_manager.launch_release(*release_to_launch);
+//     }
+//     catch (const std::exception& e)
+//     {
+//         std::cerr << "Exception occurred: " << e.what() << std::endl;
+//     }
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,17 @@
-#include <cstdlib>
-#include <iostream>
-// #include "ProjectManager/path_to_project_to_open_on_startup.hpp"
-#include <fstream>
-#include <string>
-#include "ReleaseManager.hpp"
-#include "boxer/boxer.h"
+#include "App.hpp"
+//
+#include <Cool/Core/run.h> // Must be included last otherwise it slows down compilation because it includes <ser20/archives/json.hpp>
 
 auto main(int argc, char** argv) -> int
 {
-    // TODO : faire une fonction!
-    std::string version;
-    if (argc > 1)
-    {
-        std::ifstream infile(argv[1]);
-        if (infile.is_open())
-            std::getline(infile, version);
-        infile.close();
-    }
-
-    try
-    {
-#ifdef __APPLE__
-        // Install Homebrew & FFmpeg on MacOS
-        install_macos_dependencies_if_necessary();
-#endif
-
-        ReleaseManager release_manager;
-        // release_manager.display_all_release();
-
-        Release const* release_to_launch = nullptr;
-
-        // L'utilisateur a déjà un projet sur une version différente de la latest
-        // Il a le choix de poursuivre son projet sur sa version, ou de télécharger la latest.
-        release_to_launch = release_manager.find_release(version);
-        if (release_to_launch == nullptr)
+    Cool::run<App>(
+        argc, argv,
         {
-            // std::cerr << "La version " << version << " n'a pas pu être installée, installation de la dernière version (par défaut).";
-            release_to_launch = release_manager.get_latest_release();
-            if (release_to_launch == nullptr)
-            {
-                boxer::show("Please connect to the Internet so that we can install the latest version of Coollab.\nYou don't have any version installed yet.", "You are offline");
-                return 0;
-            }
+            .windows_configs   = {{
+                  .title                  = "Coollab Launcher",
+                  .maximize_on_startup_if = false,
+            }},
+            .imgui_ini_version = 0,
         }
-
-        // Si la latest n'est pas installée -> on l'installe
-        if (!release_to_launch->is_installed())
-            release_manager.install_release(*release_to_launch);
-
-        release_manager.launch_release(*release_to_launch);
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Exception occurred: " << e.what() << std::endl;
-    }
+    );
 }

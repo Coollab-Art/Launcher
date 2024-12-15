5ba0a7d334781b8c28aab590a6c7845d0f21f373
Jules Fouchy <jules.fouchy@ntymail.com>
2024-12-15 17:24:21 +0100

    ✨ Close window after launching a project

--- src/Version/launch.cpp
+++ src/Version/launch.cpp
@@ -1,4 +1,5 @@
 #include "launch.hpp"
+#include "Cool/AppManager/close_application.hpp"
 #include "Cool/spawn_process.hpp"
 #include "installation_path.hpp"
 
@@ -8,5 +9,5 @@ void launch(VersionName const& version_name, std::optional<std::filesystem::path
         executable_path(version_name),
         project_file_path.has_value() ? std::vector<std::string>{project_file_path->string()} : std::vector<std::string>{}
     );
-    // _window.close(); // TODO(Launcher) Only close once all tasks are done (minify/hide window while we wait ?)
+    Cool::close_application_if_all_tasks_are_done(); // If some installations are still in progress we want to keep the launcher open so that they can finish. And once they are done, if we were to close the launcher it would feel weird for the user that it suddenly closed, so we just keep it open.
 }

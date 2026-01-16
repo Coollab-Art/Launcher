7b7810ac86c2c4b439faa0d9ad5ac8020d4de13a
Jules Fouchy <jules.fouchy@ntymail.com>
2026-01-16 18:47:58 +0100

    ⏪ Revert MacOS tests

--- src/Path.cpp
+++ src/Path.cpp
@@ -3,21 +3,9 @@
 
 namespace Path {
 
-#if defined(__APPLE__)
-static auto home() -> const char*
-{
-    static const char* home = std::getenv("HOME");
-    return home ? home : "";
-}
-#endif
-
 auto installed_versions_folder() -> std::filesystem::path
 {
-#if defined(__APPLE__)
-    return std::filesystem::path(home()) / "Applications/Coollab"; // On MacOS, app bundles need to be installed in ~/Applications (or /Applications) for the permissions to work properly (e.g. accessing webcam and mic) . We do ~/Applications because it's user-specific and doesn't require admin privileges to write into it, unlike /Applications
-#else
     return Cool::Path::user_data() / "Installed Versions";
-#endif
 }
 
 auto projects_info_folder() -> std::filesystem::path
